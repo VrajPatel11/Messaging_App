0a34b6157b3d62cb3a6186411dafb05e1e22cb99
VrajPatel11 <patelvraj1132000@gmail.com>
2021-09-06 16:51:33 +0530

    Active Storage(File Uploading)

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,11 @@
 class Message < ApplicationRecord
+  attr_accessor :remove_cover_picture
+  after_save :purge_cover_picture, if: :remove_cover_picture
+  private def purge_cover_picture
+    cover_picture.purge_later
+  end
+
+  has_one_attached :cover_picture
   belongs_to :user
   validates :body, presence: true
   scope :custom_display, -> { order(:created_at).last(25) }
